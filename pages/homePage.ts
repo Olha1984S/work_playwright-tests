c1b29ad4c8f3e1f8915570d8cafd172846b68e43
Olha Shynkina <o.shynkina@gmail.com>
2026-04-30 12:21:21 +0200

    Update homePage.ts

--- pages/homePage.ts
+++ pages/homePage.ts
@@ -1,1 +1,21 @@
+class HomePage {
+  constructor(page) {
+    this.page = page;
+    this.logo = page.locator('header');
+    this.menuLinks = page.locator('nav a');
+  }
 
+  async open(url) {
+    await this.page.goto(url);
+  }
+
+  async isLogoVisible() {
+    return await this.logo.isVisible();
+  }
+
+  async getMenuLinksCount() {
+    return await this.menuLinks.count();
+  }
+}
+
+module.exports = { HomePage };
