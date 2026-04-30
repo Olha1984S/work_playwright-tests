de26a4a736b415dd61cff7eb15dcfc0b98790b3d
Olha Shynkina <o.shynkina@gmail.com>
2026-04-30 12:29:14 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,16 @@
 import { test, expect } from '@playwright/test';
+import { HomePage } from '../pages/homePage';
 
-test('Простой тест — проверка заголовка', async ({ page }) => { 
-    await page.goto('https://example.com');
-    await expect(page).toHaveTitle('Example Domain');
+test('Verify that Example Domain page loads correctly', async ({ page }) => {
+  const homePage = new HomePage(page);
 
-    console.log("This is first commit in new branch");
+  // Open Example website
+  await homePage.open('https://example.com');
+
+  // Verify page title
+  await expect(page).toHaveTitle(/Example Domain/);
+
+  // Verify header is visible (basic UI check)
+  const isVisible = await homePage.isLogoVisible();
+  expect(isVisible).toBeTruthy();
 });
-
